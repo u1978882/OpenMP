83db92e7647fd5b6e51232db29b164a81463b512
u1978882 <126769186+u1978882@users.noreply.github.com>
2023-11-10 14:50:26 +0100

    producte de matrius sequencial feta

---
+++ matrius/producteMatriusSequencial.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <math.h>
+#include<malloc.h>
+
+
+void omplirMatriu(int *matriu, int tamany){
+    for (long i = 0; i < tamany; i++)
+        for (long j = 0; j < tamany; j++)
+            *((matriu+(i*(long)tamany)) + j) = rand() % 100;
+}
+
+void printMatriu(int *matriu, int tamany){
+    for (long i = 0; i < tamany; i++)
+    {
+        for (long j = 0; j < tamany; j++)
+        {
+            printf("%5i ",  *((matriu+i*tamany) + j));
+        }
+        printf("\n");
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+
+    int tamanyMatriu = 100;
+    if (argc >= 2) tamanyMatriu = atoi(argv[1]);
+
+    int *matriuA = malloc(sizeof(int) * tamanyMatriu * tamanyMatriu);
+    int *matriuB = malloc(sizeof(int) * tamanyMatriu * tamanyMatriu);
+    int *matriuC = malloc(sizeof(int) * tamanyMatriu * tamanyMatriu);
+
+    if (matriuA == NULL || matriuB == NULL || matriuC == NULL) {
+        fprintf(stderr, "Ha fallat el malloc!\n");
+        free(matriuA);
+        free(matriuB);
+        free(matriuC);
+        return 0;
+    }
+    
+    omplirMatriu(matriuB, tamanyMatriu);
+    omplirMatriu(matriuA, tamanyMatriu);
+
+  	
+    clock_t clock_inicial = clock();
+
+    // Multiplicacio matriu
+    for (long i = 0; i < tamanyMatriu; i++)
+    {
+        for (long j = 0; j < tamanyMatriu; j++)
+        {
+            *((matriuC+i*tamanyMatriu) + j) = 0;
+            for (long k = 0; k < tamanyMatriu; k++)
+            {
+                *((matriuC+i*tamanyMatriu) + j) += *((matriuA+i*tamanyMatriu) + k) * *((matriuB+k*tamanyMatriu) + j);
+            }
+        }
+    }
+    
+    clock_t clock_final = clock();
+    printf("%f\n",(double)(clock_final - clock_inicial) / CLOCKS_PER_SEC);
+    
+    //printf("--- Matriu A ---\n");
+    //printMatriu(matriuA, tamanyMatriu);
+    //printf("--- Matriu B ---\n");
+    //printMatriu(matriuB, tamanyMatriu);
+    //printf("--- Matriu C ---\n");
+    //printMatriu(matriuC, tamanyMatriu);
+
+    free(matriuA);
+    free(matriuB);
+    free(matriuC);  
+
+	return 0;
+}
+
